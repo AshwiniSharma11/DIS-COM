5fc0f5956a723b065490cf4e90d242f289cfa5da
Ashwini Sharma <itsashwinisharma@gmail.com>
2024-12-31 01:02:39 +0530

    Update activate.fish

--- discom-env/bin/activate.fish
+++ discom-env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/omkar/Documents/Project/discom/discom-env"
+set -gx VIRTUAL_ENV "/home/ashwini/Documents/Project/discom/discom-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
